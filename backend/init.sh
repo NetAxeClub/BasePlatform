86b3ec95972c26c907aabcdf976b04a0c3ca52dc
M87NET <M87NET@github.com>
2023-06-02 09:56:55 +0800

    删除旧版本配置文件

--- backend/init.sh
+++ backend/init.sh
@@ -7,16 +7,8 @@
 # pip3 install -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple/
 python3 manage.py makemigrations users
 python3 manage.py migrate users
+python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py makemigrations automation
-python3 manage.py migrate automation
-python3 manage.py makemigrations asset
-python3 manage.py migrate asset
-python3 manage.py makemigrations int_utilization
-python3 manage.py migrate int_utilization
-python3 manage.py makemigrations topology
-python3 manage.py migrate topology
-
 python3 manage.py init_asset # 资产初始化
 python3 manage.py init_collect # 采集方案初始化
 #python3 manage.py init_system_menu # 系统菜单初始化
