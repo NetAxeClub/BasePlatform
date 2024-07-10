fff1f8cc6ba5be5ce694c90691606119c0012e0f
M87NET <M87NET@github.com>
2024-07-10 23:54:37 +0800

    feature: 增加配置备份接口逻辑2

--- backend/apps/config_center/views.py
+++ backend/apps/config_center/views.py
@@ -442,18 +442,7 @@ class ConfigFileView(APIView):
 
     def post(self, request):
         post_data = request.data
-<<<<<<< HEAD
-<<<<<<< HEAD
-        print(post_data)
-        # file_path = f"current-configuration/{post_data['manage_ip']}/{post_data['manage_ip']}.txt"
-        file_path = f"current-configuration/10.254.30.30/hp_comware-10.254.30.30.txt"
-        get_file_commit = _ConfigGit.get_file_commit_filter_by_date(file_path, post_data["last_time"].split(' ')[0])
-=======
         get_file_commit = _ConfigGit.get_file_content_by_commit(post_data['file_path'], post_data["commit"])
->>>>>>> 7c46413f37a900620783da4ddab4da9840d899b5
-=======
-        get_file_commit = _ConfigGit.get_file_content_by_commit(post_data['file_path'], post_data["commit"])
->>>>>>> 0c254d08ae6d937b64120deea702a3025644e1e8
         if get_file_commit:
             data = {
                 "code": 200,
