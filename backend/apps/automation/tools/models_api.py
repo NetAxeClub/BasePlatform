4e73dd251bab71d71cfebf84768db0a30f3a1a59
jmli12 <jmli12@iflytek.com>
2024-07-31 10:07:04 +0800

    bugfix: 优化获取防火墙设备逻辑

--- backend/apps/automation/tools/models_api.py
+++ backend/apps/automation/tools/models_api.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # 2023/8/28
-from django.db import connections
+# from django.db import connections
 from apps.asset.models import NetworkDevice, Category
+from apps.asset.serializers import NetworkDeviceSerializer
 
 
 def get_firewall_list(manage_ip_list=None):
-    connections.close_all()
+    # connections.close_all()
     category = Category.objects.filter(name='防火墙').values('id').first()
-    all_devs = []
     # 获取所有cmdb设备
     if manage_ip_list:
         all_devs = NetworkDevice.objects.filter(auto_enable=True,
@@ -20,7 +20,7 @@ def get_firewall_list(manage_ip_list=None):
                                                 status=0, category=category['id'], ha_status__in=[0, 1]).select_related(
             'vendor', 'category').prefetch_related('bind_ip').values(
             'id', 'manage_ip', 'name', 'vendor__alias', 'bind_ip__ipaddr', 'soft_version')
-    return all_devs
+    return list(all_devs)
 
 
 def get_device_info(**kwargs):
