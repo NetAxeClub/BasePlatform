cebb3255ff41096fc9a2b0077b3c431f1ccf68c5
jmli12 <jmli12@iflytek.com>
2024-08-16 16:27:58 +0800

    bugfix: 自动化工作流增加admin视图

--- backend/apps/automation/admin.py
+++ backend/apps/automation/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import CollectionPlan, CollectionRule, CollectionMatchRule
+from .models import CollectionPlan, CollectionRule, CollectionMatchRule, AutoFlow
 
 # Register your models here.
 admin.site.register(CollectionPlan)
 admin.site.register(CollectionRule)
+admin.site.register(AutoFlow)
 admin.site.register(CollectionMatchRule)
