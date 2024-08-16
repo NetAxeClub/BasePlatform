54d4f09d13c90afa5d61138cc26c1900625959a1
jmli12 <jmli12@iflytek.com>
2024-08-16 16:32:35 +0800

    bugfix: 自动化工作流增加admin视图2

--- backend/apps/automation/admin.py
+++ backend/apps/automation/admin.py
@@ -4,5 +4,10 @@ from .models import CollectionPlan, CollectionRule, CollectionMatchRule, AutoFlo
 # Register your models here.
 admin.site.register(CollectionPlan)
 admin.site.register(CollectionRule)
-admin.site.register(AutoFlow)
 admin.site.register(CollectionMatchRule)
+
+
+@admin.register(AutoFlow)
+class FSMModelAdmin(admin.ModelAdmin):
+    list_display = ['task_id', 'device', 'commit_user', 'commit_time', 'task', 'kwargs', 'state', 'task_result']
+    search_fields = ['task_id', 'device', 'commit_user', 'commit_time', 'task', 'kwargs', 'state', 'task_result']
