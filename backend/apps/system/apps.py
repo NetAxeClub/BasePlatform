b24ad7db8b14c2bafa7da119e2ad0723ff6a9fd9
jmli12 <jmli12@iflytek.com>
2024-09-13 11:01:02 +0800

    bugfix: 优化nacos注册2

--- backend/apps/system/apps.py
+++ backend/apps/system/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+from utils.nacos_register import nacos_init
 
 class SystemConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.system'
+
+    def ready(self):
+        nacos_init()
