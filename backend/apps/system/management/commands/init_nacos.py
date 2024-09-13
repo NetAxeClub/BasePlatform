31a53dfb37a0fa59e7637602cd6a7c8500436137
jmli12 <jmli12@iflytek.com>
2024-09-13 11:01:02 +0800

    bugfix: 优化nacos注册2

--- backend/apps/system/management/commands/init_nacos.py
+++ backend/apps/system/management/commands/init_nacos.py
@@ -10,13 +10,4 @@
                     2022/11/4 20:34
 -------------------------------------------------
 """
-from utils.custom.nacos import nacos
-from netaxe.conf import SERVERIP, SERVERPORT
-# 注册服务
-nacosServer = nacos(ip=SERVERIP, port=8848)
-nacosServer.registerService(
-    serviceIp=SERVERIP,
-    servicePort=SERVERPORT,
-    serviceName="auth",
-    groupName="default")
-nacosServer.healthyCheck()
+
