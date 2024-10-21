640e6241cd0daa698c219f15e1013b01bcc6900d
xhweng2 <xhweng2@iflytek.com>
2024-10-21 15:58:18 +0800

    适配windows

--- backend/manage.py
+++ backend/manage.py
@@ -15,8 +15,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    # if os.name == 'nt':  # 如果是Windows系统
-    #     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    if os.name == 'nt':  # 如果是Windows系统
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
     execute_from_command_line(sys.argv)
 
