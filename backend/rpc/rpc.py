00206883d8295f8c9bc0ddeac435b0ec8cd73cc1
jmli12 <jmli12@iflytek.com>
2025-01-02 00:23:34 +0800

    feature: 增加服务间访问方法

--- backend/rpc/rpc.py
+++ backend/rpc/rpc.py
@@ -59,6 +59,7 @@ def dispatcher(method, data):
                 res.forget()
             return [{'task_id': str(res)}]
         elif method == 'get_device_info':
+            log.info(data)
             return get_device_info(**data)
         else:
             _FirewallMain = FirewallMain(data['host'])
