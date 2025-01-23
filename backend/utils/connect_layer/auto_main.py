99a4747ca450742f99f22a6a4221b251ca1c7680
junhu19 <junhu19@iflytek.com>
2025-01-23 14:10:20 +0800

    添加测试路由文件

--- backend/utils/connect_layer/auto_main.py
+++ backend/utils/connect_layer/auto_main.py
@@ -302,7 +302,6 @@ class BatManMain(object):
             cmd = ' '.join(_cmd.split('_'))
             _content = default_storage.open(path).read()
             _content = _content.decode('utf-8')
-            # print(cmd)
             res = BatManFsm.get_structured_data(platform=fsm_platform,
                                                 command=cmd,
                                                 raw_output=_content)
@@ -1544,7 +1543,8 @@ if __name__ == '__main__':
     # res = BatManMain.info_fsm(path='automation/huawei/display_interface.txt', fsm_platform='huawei_vrp')
     # res = BatManMain.info_fsm(path='automation/huawei/display_eth-trunk.txt', fsm_platform='huawei_vrp')
     # res = BatManMain.info_fsm(path='automation/huawei/display_lldp_neighbor.txt', fsm_platform='huawei_vrp')
-    res = BatManMain.info_fsm(path='automation/huawei/display_version.txt', fsm_platform='huawei_vrp')
+    # res = BatManMain.info_fsm(path='automation/huawei/display_version.txt', fsm_platform='huawei_vrp')
+    res = BatManMain.info_fsm(path='route_file/display_ip_routing-table.txt', fsm_platform='hp_comware')
     # res = BatManMain.info_fsm(path='automation/10.1.1.2/show_lldp_neighbors_detail.txt', fsm_platform='cisco_ios')
     if isinstance(res, list):
         for i in res:
