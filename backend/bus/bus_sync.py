e2b3942f053da30f09d00427243fc5c765624d73
jmli12 <jmli12@iflytek.com>
2025-02-26 16:23:38 +0800

    test: 测试队列的自动删除功能1

--- backend/bus/bus_sync.py
+++ backend/bus/bus_sync.py
@@ -210,7 +210,7 @@ class SyncMessageBus:
         connection.process_data_events(time_limit=None)
         return int(self.response)
 
-    def rpc_server(self, queue, routing_key, callback, durable=True, auto_delete=False):
+    def rpc_server(self, queue, routing_key, callback, durable=True, auto_delete=True):
         channel = self.get_channel()
         arguments = {"x-max-priority": 10}
         channel.exchange_declare(
