00c782bc08b576d56be8d63f5fc1586ada2a47cd
jmli12 <jmli12@iflytek.com>
2025-03-21 17:32:32 +0800

    feature: 增加接口利用率配置

--- backend/apps/automation/tasks.py
+++ backend/apps/automation/tasks.py
@@ -369,6 +369,19 @@ def interface_used(device_ip=None):
                 continue
             elif i['interface'].startswith('AggregatePort'):
                 continue
+            elif i['interface'].startswith('Eth-Trunk'):
+                continue
+            # Virtual-if0 NULL  vlan  mgmt_eth  null  smartgroup
+            elif i['interface'].startswith('Virtual-if'):
+                continue
+            elif i['interface'].startswith('NULL'):
+                continue
+            elif i['interface'].startswith('vlan'):
+                continue
+            elif i['interface'].startswith('null'):
+                continue
+            elif i['interface'].startswith('smartgroup'):
+                continue
             _tmp_slot = i['interface'].split('/')[0]
             mongo_res_slot.append(int(_tmp_slot[-1]))
         mongo_res_slot = list(set(mongo_res_slot))
