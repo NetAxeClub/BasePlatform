60888f70a09f0a676e66d834c37d06fb26e7f99c
jmli12 <jmli12@iflytek.com>
2025-04-02 19:52:03 +0800

    配置合规检查问题修复

--- backend/apps/config_center/tasks.py
+++ backend/apps/config_center/tasks.py
@@ -206,9 +206,9 @@ def config_compliance(**kwargs):
 
     for config_file in config_files:
         if config_file.vendor in vendor_map:
-            if not default_storage.exists(config_file.file_path):
+            if not default_storage.exists(f"device_config/{config_file.file_path}"):
                 continue
-            data_to_parse = default_storage.open(config_file.file_path).read().decode('utf-8')
+            data_to_parse = default_storage.open(f"device_config/{config_file.file_path}").read().decode('utf-8')
             rules = ConfigComplianceRule.objects.all().iterator()
             for rule in rules:
                 childrens = rule.children.all()
