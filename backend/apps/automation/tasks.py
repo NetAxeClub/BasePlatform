89ffa10e29c2df73fa6247e90790ea76bf12f289
jmli12 <jmli12@iflytek.com>
2025-08-04 17:08:45 +0800

    增加寻觅过期数据清理机制

--- backend/apps/automation/tasks.py
+++ backend/apps/automation/tasks.py
@@ -1360,6 +1360,7 @@ def tracking_main():
     interface_used.apply_async()
     xunmi_process_mongo.delete_many()
     total_records = total_ip_mongo.count_documents()
+    MongoNetOps.clear_xunmi()
     # bus = SyncMessageBus()
     page_size = 20
     total_pages = math.ceil(total_records / page_size)
