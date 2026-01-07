2425f2d662f0ed77cbd5fcdd828a0605dd9886e4
jmli12 <jmli12@iflytek.com>
2026-01-07 15:56:42 +0800

    内网全量地址库排除掉公网地址

--- backend/apps/automation/tasks.py
+++ backend/apps/automation/tasks.py
@@ -517,7 +517,7 @@ def standard_analysis_main():
     #     result.setdefault(item, {**item})
     # xunmi_res = list(result.values())
     # xunmi_res = [dict(ipaddress=x) for x in total_ip_tmp if not x.startswith('10.254.')]
-    xunmi_res = [dict(ipaddress=x) for x in total_ip_tmp]
+    xunmi_res = [dict(ipaddress=x) for x in total_ip_tmp if netaddr.IPAddress(x).is_private()]
     # 所有IP明细存入mongo, 作为后面地址定位源数据
     total_ip_mongo = MongoOps(db='Automation', coll='Total_ip_list')
     total_ip_mongo.delete()
