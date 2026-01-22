d9490f47f1649bd8472e3ee5e9304eb16f3d1681
jmli12 <jmli12@iflytek.com>
2026-01-22 10:15:13 +0800

    优化寻觅任务

--- backend/apps/automation/tasks.py
+++ backend/apps/automation/tasks.py
@@ -516,7 +516,8 @@ def standard_analysis_main():
     # for item in total_ip_tmp:
     #     result.setdefault(item, {**item})
     # xunmi_res = list(result.values())
-    xunmi_res = [dict(ipaddress=x) for x in total_ip_tmp if not x.startswith('10.254.')]
+    # xunmi_res = [dict(ipaddress=x) for x in total_ip_tmp if not x.startswith('10.254.')]
+    xunmi_res = [dict(ipaddress=x) for x in total_ip_tmp]
     # xunmi_res = [dict(ipaddress=x) for x in total_ip_tmp if netaddr.IPAddress(x).is_private()]
     # 所有IP明细存入mongo, 作为后面地址定位源数据
     total_ip_mongo = MongoOps(db='Automation', coll='Total_ip_list')
