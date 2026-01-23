13274866dbd4643ebf6f7e7493537e04a1959b6d
jmli12 <jmli12@iflytek.com>
2026-01-23 11:00:55 +0800

    删除无效配置和文件

--- backend/init.sh
+++ backend/init.sh
@@ -13,8 +13,9 @@ python3 manage.py makemigrations int_utilization
 python3 manage.py migrate int_utilization
 python3 manage.py makemigrations topology
 python3 manage.py migrate topology
-python3 manage.py makemigrations open_ipam
-python3 manage.py migrate open_ipam
+# open_ipam app 不存在，已注释
+# python3 manage.py makemigrations open_ipam
+# python3 manage.py migrate open_ipam
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py init_asset # 资产初始化
